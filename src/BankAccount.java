30db9862a4b1a39ed6a5f0b28604f2d618164f98
hannahlamas <81644186+hannahlamas@users.noreply.github.com>
2025-01-31 18:11:36 +0000

    third commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,16 @@
+public class BankAccount {
+    public static void main(String[] args){
 
+        double balance = 2175.37;
+        balance -= 302.50;
+        balance += 50.03;
+        balance /= 2;
+        balance += 20.00;
+        balance -= 1.00;
+        balance *= 2;
+        balance += 1.00;
+
+        System.out.printf("Bob's balance: $%.2f\n", balance);
+    
+    }
+}
